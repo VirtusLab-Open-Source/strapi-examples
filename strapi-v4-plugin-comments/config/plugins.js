8572efe23e6f6d7f89829230ed232851757741d6
Mateusz Ziarko <mateusz.ziarko@tesco.com>
2022-04-22 11:58:28 +0200

    chore: bump versions

--- strapi-v4-plugin-comments/config/plugins.js
+++ strapi-v4-plugin-comments/config/plugins.js
@@ -1,18 +1,4 @@
 module.exports = {
-  'graphql': {
-    enabled: true,
-    config: {
-      endpoint: '/graphql',
-      shadowCRUD: true,
-      playgroundAlways: false,
-      depthLimit: 7,
-      amountLimit: 100,
-      apolloServer: {
-        tracing: false,
-      },
-    },
-  },
-  'comments': {
-      enabled: true,
-    },
+  'comments': { enabled: true },
+  'graphql': { enabled: true },
 };
