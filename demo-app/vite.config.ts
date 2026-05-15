adc3b6725eea18fc7888f8441b3e5a709d9181a1
Trochonovitz <mat.trochonowicz@gmail.com>
2026-05-15 19:34:03 +0200

    feat: add init demo-app boilerplate

---
+++ demo-app/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    react(),
+  ],
+})
